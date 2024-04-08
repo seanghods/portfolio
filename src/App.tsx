42388b03142ae25166c6c9d215840eb7f0b3596c
Sean Ghods <137848705+seanghods@users.noreply.github.com>
2024-04-07 20:42:11 -0700

    remove theme toggler

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import { Intro } from "./components/Intro.tsx";
 import { Projects } from "@/components/Projects.tsx";
 import { Topbar } from "@/components/Topbar.tsx";
 import { Resume } from "@/components/Resume.tsx";
-import { ThemeToggler } from "@/components/ThemeToggler.tsx";
-import { Theme, useTheme } from "react-daisyui";
+import { Theme } from "react-daisyui";
 import { Skills } from "./components/Skills.tsx";
 import { Contact } from "./components/Contact.tsx";
 
 function App() {
-  const { theme } = useTheme();
-
   return (
     <>
       <Theme dataTheme={"dark"}>
@@ -19,7 +16,6 @@ function App() {
         <Projects />
         <Resume />
         <Contact />
-        <ThemeToggler />
       </Theme>
     </>
   );
